97b2490847b21672bb579932c23c491ac3c53b65
Luis C. <luis.cornejo.u@hotmail.com>
2023-08-24 16:55:54 -0400

    inicie nuevo repo

--- DOM 1/js/app.js
+++
@@ -1,9 +0,0 @@
-const inputColor = document.getElementById("inputColor");
-const btnVisualizar = document.getElementById("btnVisualizar");
-const contenedorColor = document.getElementById("contenedorColor");
-const numColor = document.getElementById("numColor");
-
-btnVisualizar.addEventListener("click", ()=>{
-    numColor.textContent = inputColor.value;
-    contenedorColor.style.backgroundColor = inputColor.value;
-})
